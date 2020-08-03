7e66c51aa74dd21d11f5e63e5c35faf4c91ccafd
nokmal <9aedarae@gmail.com>
2020-08-03 21:48:34 +0900

    서블릿게시판1 - 구구단 구현

---
+++ src/com/sbs/java/am/HomePrintDanServlet.java
@@ -0,0 +1,36 @@
+package com.sbs.java.am;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home/printDan")
+public class HomePrintDanServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String inputedDan = request.getParameter("dan"); 
+		
+		if ( inputedDan == null) {
+			inputedDan = "1";
+		}
+		
+		String inputedLimit = request.getParameter("limit"); 
+		
+		if ( inputedLimit == null) {
+			inputedLimit = "9";
+		}
+		
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+		response.getWriter().append(String.format("%d단<br>", dan));
+		for (int i = 1; i <= limit ; i++) {
+			response.getWriter().append(String.format( "%d * %d = %d<br>", dan, i, dan * i));
+		}
+	}	
+}
